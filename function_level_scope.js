3d0a7743186536c3d9edafa5bc1cfc1918d7d1b1
USER-PC\USER <weg901127@naver.com>
2018-09-10 13:57:23 +0900

    commit

---
+++ function_level_scope.js
@@ -0,0 +1,12 @@
+var time = 10;
+var greeting;
+
+if(time<10){
+    greeting = 'Good morning';
+}else if(time < 20){
+    greeting = 'Good day';
+}else{
+    greeting = 'Good evening';
+}
+
+console.log(greeting);
