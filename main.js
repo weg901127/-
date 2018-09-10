74c3cf2133d933ae1bd6eeaf890913ddc39bd05d
USER-PC\USER <weg901127@naver.com>
2018-09-10 11:46:04 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+var heading = document.getElementById('heading');
+var myButton = document.getElementById('my-btn');
+
+myButton.addEventListener('click',function(){heading.innerHTML='Hello World!';
+});
